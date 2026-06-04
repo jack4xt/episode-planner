217469b971d8d1313eaa88c3771b9db9cdefd49b
jack4xt <jantriska@pm.me>
2026-06-04 11:25:16 +0200

    Add ABI splits for IzzyOnDroid

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,7 +49,14 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
+    splits {
+    abi {
+        isEnable = true
+        reset()
+        include("arm64-v8a", "armeabi-v7a")
+        isUniversalApk = false
+         }
+     }
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
